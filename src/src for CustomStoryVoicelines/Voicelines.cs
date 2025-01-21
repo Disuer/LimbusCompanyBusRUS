beb6f963960a0d6cdcb4cc0547881e58d705fa49
Disuer <123813985+Disuer@users.noreply.github.com>
2025-01-21 17:47:18 +0500

    update src for CustomVoicelines

--- src/src for CustomStoryVoicelines/Voicelines.cs
+++ src/src for CustomStoryVoicelines/Voicelines.cs
@@ -21,11 +21,27 @@ namespace LimbusCompanyBusRUS
         public static float voiceVolume;
         public static float masterVolume;
         public static ChannelGroup channelGroup;
-        //PlayBasicVoice, PlayLoginVoice, PlayLobbyVoice, PlayLobbyVoice, GetLobbyVoice, PlaySmallTalkVoice, PlayNeglectVoice, PlayFormationVoice, PlayGetVoice
-        //PlayGacksungVoice, PlayBattleEntryVoice, PlayBattleStartStageVoice, PlayBattleSelectVoice, PlayBattleBreakVoice, PlayBattleAllyDeadVoice, PlayBattleDeadVoice
-        //PlayBattleEnemyBreakVoice, PlayBattleKillVoice, PlayBattleEndCommandVoice, PlaySkillVoice, PlayBattleLoseduelVoice, PlayChoiceSucceesPossitiveVoice, PlayChoiceSucceesNegativeVoice
-        //PlayChoiceFailPossitiveVoice, PlayChoiceFailNegativeVoice, PlayBattleClearEXVoice, PlayBattleClearVoice, PlayBattleDefeatVoice, PlaySpecialVoice, PlayEGOAwakenVoice, PlayEGOErosionVoice
-        //PlayAnnouncerBasicVoice, PlayAnnouncerEquipVoice, PlayAnnouncerAdvAtkPhysicalVoice, PlayAnnouncerAdvAtkAttrVoice, PlayAnnouncerDisadvAtkPhysicalVoice, PlayAnnouncerDisdvAtkAttrVoice
+        [HarmonyPatch(typeof(VoiceGenerator), nameof(VoiceGenerator.CreateVoiceInstance))]
+        [HarmonyPrefix]
+        private static bool CreateVoiceInstance(string path, bool isSpecial)
+        {
+            LCB_CresCorpMod.LogInfo($"{path} : {isSpecial}");
+            string pathus = $"{LCB_CresCorpMod.ModPath}\\Voicelines\\{path.Substring(7)}.wav";
+            LCB_CresCorpMod.LogInfo(pathus);
+            FMOD.RESULT result = RuntimeManager.CoreSystem.createSound(pathus, FMOD.MODE.CREATESTREAM, out FMOD.Sound sound);
+            if (result == FMOD.RESULT.OK)
+            {
+                VoiceGenerator.StopVoiceSound();
+                float overallVolume = masterVolume * voiceVolume;
+                channelGroup.stop();
+                channelGroup.setVolume(overallVolume);
+                RuntimeManager.CoreSystem.playSound(sound, channelGroup, false, out FMOD.Channel channel);
+                sound.setMusicChannelVolume(1, overallVolume);
+                channel.setVolume(overallVolume);
+                return false;
+            }
+            return true;
+        }
         [HarmonyPatch(typeof(LoadingSceneManager), nameof(LoadingSceneManager.SetHintText))]
         [HarmonyPrefix]
         private static void LoadingSceneManager_Init14(LoadingSceneManager __instance)
@@ -33,44 +49,6 @@ namespace LimbusCompanyBusRUS
             RuntimeManager.CoreSystem.createChannelGroup("123", out channelGroup);
             RuntimeManager.CoreSystem.getMasterChannelGroup(out var group);
             group.stop();
-        }
-        [HarmonyPatch(typeof(StorySoundController), nameof(StorySoundController.CallVoice))]
-        [HarmonyPostfix]
-        public static void StorySoundController2(StorySoundController __instance, ref string code, ref string storyID)
-        {
-            string name = Regex.Replace(code, "\\s", "").Split(':', StringSplitOptions.None)[0];
-            if (code.Contains(storyID[..2]))
-            {
-                PlayStoryVoice_Custom(name, storyID);
-                return;
-            }
-            PlayStoryVoice_Custom(name, null);
-        }
-        public static void PlayStoryVoice_Custom(string name, string storyID)
-        {
-            VoiceGenerator.StopVoiceSound();
-            if (storyID != null)
-            {
-                if (localizedStory.Contains(storyID))
-                {
-                    FMOD.RESULT result = RuntimeManager.CoreSystem.createSound(LCB_CresCorpMod.GamePath + $"\\LimbusCompany_Data\\Voicelines\\1D101A\\{name}.wav", FMOD.MODE.DEFAULT, out FMOD.Sound sound);
-                    if (result == FMOD.RESULT.OK)
-                    {
-                        float overallVolume = masterVolume * voiceVolume;
-                        channelGroup.stop();
-                        channelGroup.setVolume(overallVolume);
-                        RuntimeManager.CoreSystem.playSound(sound, channelGroup, false, out FMOD.Channel channel);
-                        sound.setMusicChannelVolume(1, overallVolume);
-                        channel.setVolume(overallVolume);
-                    }
-                }
-                else
-                {
-                    VoiceGenerator.SetMainVoice(VoiceGenerator.CreateVoiceInstance(VoiceGenerator.VOICE_STORY_EVENT_PATH + storyID + "/" + name, false), -1);
-                }
-                return;
-            }
-            VoiceGenerator.SetMainVoice(VoiceGenerator.CreateVoiceInstance(VoiceGenerator.VOICE_EVENT_PATH + "Default/" + name, false), -1);
         }
         [HarmonyPatch(typeof(SettingsPanelSounds), nameof(SettingsPanelSounds.RefreshVolumes))]
         [HarmonyPostfix]
